36fa692f55aefce16e129e16fcfcdfea0d5aa08f
mohamed <mrmohamedmoustafa@gmial.com>
2022-01-05 22:29:22 +0300

    starter react project

---
+++ client/gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require('gulp');
+const gulpSass = require('gulp-sass');
+const sass = gulpSass(require('sass'));
+
+
+gulp.task('sass', async function(){
+    gulp.watch('src/*.scssstop', function(){
+        gulp.src('src/*.scss').pipe(sass()).pipe(gulp.dest('src/css'))
+    })
+   
+})
---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App">
+     shooping cart
+    </div>
+  );
+}
+
+export default App;
